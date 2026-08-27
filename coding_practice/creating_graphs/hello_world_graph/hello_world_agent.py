4c2b3bef7f178908993ea1debe6af3cc22e5d3a6
Aditya Kumar <aryan68125@gmail.com>
2026-08-27 11:41:06 +0530

    Updated the code and added an empty graph and added a node in that graph

--- coding_practice/creating_graphs/hello_world_graph/hello_world_agent.py
+++ coding_practice/creating_graphs/hello_world_graph/hello_world_agent.py
@@ -2,6 +2,17 @@ from typing import Dict, TypedDict
 # This is the framework that allows you design and manage the flow of tasks in your application using graph
 from langgraph.graph import StateGraph 
 
-# Here we now will create an AgentState 
-# - A shared data structure that keeps track of information as your application runs.
+# Here we now will create an AgentState - A shared data structure that keeps track of information as your application runs.
+
+# Create a State
+# In LangGraph we create States using classes 
+class AgentState(TypedDict):
+    message : str
+
+# Create a Node
+def greeting_node(state : AgentState) -> AgentState:
+    # Docs string are very important in case of langraph 
+    # these docs strings will tell the llm what this function actually does
+    # what actions does this function performs
+
 

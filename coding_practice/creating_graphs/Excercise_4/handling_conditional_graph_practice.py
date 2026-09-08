a4c0d9ae0a54f43a020899c094ccf4441e7179b1
Aditya Kumar <aryan68125@gmail.com>
2026-09-08 12:34:02 +0530

    Fixed the import error

--- coding_practice/creating_graphs/Excercise_4/handling_conditional_graph_practice.py
+++ coding_practice/creating_graphs/Excercise_4/handling_conditional_graph_practice.py
@@ -1,6 +1,6 @@
 from typing import TypedDict, List
 
-from langgraph import StateGraph, START, END
+from langgraph.graph import StateGraph, START, END
 
 import io
 import matplotlib.pyplot as plt
@@ -130,7 +130,7 @@ print(answers)
 # Now I am going the run the compiled graph using .stream() function
 print("Using .stream() function to run the compiled graph : ")
 print("\n")
-for step in compiled_graph.stream({"num1":num1,"num2":num2,"operator":operator}):
+for step in compiled_graph.stream({"num1":num1,"num2":num2,"operator":operator})
     print(step)
 
 

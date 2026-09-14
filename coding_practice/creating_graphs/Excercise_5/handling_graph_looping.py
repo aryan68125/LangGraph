d7f33d725f42b1f044c93ad1c509db74313a9b21
Aditya Kumar <aryan68125@gmail.com>
2026-09-14 11:53:09 +0530

    Added state agent in the program

--- coding_practice/creating_graphs/Excercise_5/handling_graph_looping.py
+++ coding_practice/creating_graphs/Excercise_5/handling_graph_looping.py
@@ -1,3 +1,13 @@
-print(f"This is a test program!")
+from langgraph.graph import StateGraph , START, END
+import random
+from typing import Dict, List , TypedDict
 
+class AgentState(TypedDict):
+    name : str
+    number : List[int]
+    counter : int
 
+def GreetingNode : 
+    def __call__(self, state : AgentState) -> AgentState:
+        """Greeting Node which says hi to the person"""
+        state("name")
